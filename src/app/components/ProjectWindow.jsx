b31d4a0a34a8ed4ea3505e6973d6c75b69c2b407
OscarRapale <oscarrapale@gmail.com>
2025-08-21 14:23:33 -0400

    Implement code changes to enhance functionality and improve performance

--- src/app/components/ProjectWindow.jsx
+++ src/app/components/ProjectWindow.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./ProjectWindow.module.css";
 
 const ProjectWindow = () => {
@@ -35,52 +36,89 @@ const ProjectWindow = () => {
             </ul>
           </div>
 
-          <div className="work">
+          <div className={styles.work}>
             <h2>Projects</h2>
-            <div className="theLine">
-              <h2>The Line - Tribute to The Bear TV Series</h2>
-              <img src="" alt="" />
-              <p>
-                A modern, responsive restaurant website for "The Line", a fine
-                dining establishment in Chicago's River North neighborhood.
-                Built with vanilla HTML, CSS, and JavaScript, featuring elegant
-                design, smooth animations, and comprehensive responsive layouts.
-                A tribute to the tv show "The Bear". <br />
-                <a href="">Visit The Line</a>
-              </p>
+            <div className={styles.theLine}>
+              <div className={styles.projectImage}>
+                <Image
+                  src="/images/the-line.webp"
+                  alt="Screen capture of The Line Homepage"
+                  width={300}
+                  height={250}
+                />
+              </div>
+              <div className={styles.projectContent}>
+                <h3>The Line - Tribute to The Bear TV Series</h3>
+                <p>
+                  A modern, responsive restaurant website for "The Line", a fine
+                  dining establishment in Chicago's River North neighborhood.
+                  Built with vanilla HTML, CSS, and JavaScript, featuring
+                  elegant design, smooth animations, and comprehensive
+                  responsive layouts. A tribute to the tv show "The Bear".{" "}
+                  <br />
+                  <a href="">Visit The Line</a>
+                </p>
+              </div>
             </div>
-            <div className="shellfolio">
-              <h2>Shell Style Porfolio</h2>
-              <img src="" alt="" />
-              <p>
-                A portfolio website that simulates a terminal interface, built
-                with vanilla JavaScript, HTML, and CSS. Experience my
-                professional information through command-line interactions!{" "}
-                <br />
-                <a href="">Visit my Shell Style Portfolio</a>
-              </p>
+            <div className={styles.shellfolio}>
+              <div className={styles.projectImage}>
+                <Image
+                  src="/images/the-line.webp"
+                  alt="Screen capture of Shell Portfolio"
+                  width={300}
+                  height={250}
+                />
+              </div>
+              <div className={styles.projectContent}>
+                <h3>Shell Style Portfolio</h3>
+                <p>
+                  A portfolio website that simulates a terminal interface, built
+                  with vanilla JavaScript, HTML, and CSS. Experience my
+                  professional information through command-line interactions!{" "}
+                  <br />
+                  <a href="">Visit my Shell Style Portfolio</a>
+                </p>
+              </div>
             </div>
-            <div className="simplePortfolio">
-              <h2>Simple HTML/CSS Porfolio</h2>
-              <img src="" alt="" />
-              <p>
-                This is my personal portfolio website built with HTML, designed
-                to present my work and capabilities as a developer. The
-                portfolio serves as a digital resume and project showcase.
-                Including smooth animations. <br />
-                <a href="">Visit My Portfolio</a>
-              </p>
+            <div className={styles.simplePortfolio}>
+              <div className={styles.projectImage}>
+                <Image
+                  src="/images/the-line.webp"
+                  alt="Screen capture of The Line Homepage"
+                  width={300}
+                  height={250}
+                />
+              </div>
+              <div className={styles.projectContent}>
+                <h3>Simple HTML/CSS Portfolio</h3>
+                <p>
+                  This is my personal portfolio website built with HTML,
+                  designed to present my work and capabilities as a developer.
+                  The portfolio serves as a digital resume and project showcase.
+                  Including smooth animations. <br />
+                  <a href="">Visit My Portfolio</a>
+                </p>
+              </div>
             </div>
-            <div className="janCuts">
-              <h2>Jan Cuts Portfolio</h2>
-              <img src="" alt="" />
-              <p>
-                A portfolio website that was requested by a professional barber
-                that wanted to showcase his work and skills and the services he
-                is offering. The website is made with React.js and Chakra UI,
-                and the UI was designed with Figma. <br />
-                <a href="">Visit Jan Cuts</a>
-              </p>
+            <div className={styles.jancuts}>
+              <div className={styles.projectImage}>
+                <Image
+                  src="/images/the-line.webp"
+                  alt="Screen capture of The Line Homepage"
+                  width={300}
+                  height={250}
+                />
+              </div>
+              <div className={styles.projectContent}>
+                <h3>Jan Cuts Portfolio</h3>
+                <p>
+                  A portfolio website that was requested by a professional
+                  barber that wanted to showcase his work and skills and the
+                  services he is offering. The website is made with React.js and
+                  Chakra UI, and the UI was designed with Figma. <br />
+                  <a href="">Visit Jan Cuts</a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
