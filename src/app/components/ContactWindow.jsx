ba6c82da93a120c72a796bd9faf89cf8ef12569e
OscarRapale <oscarrapale@gmail.com>
2025-08-22 14:09:29 -0400

    Refactor ContactWindow component: update layout with CSS modules, enhance styling, and fix background color

--- src/app/components/ContactWindow.jsx
+++ src/app/components/ContactWindow.jsx
@@ -1,20 +1,31 @@
 import React from "react";
+import styles from "./ContactWindow.module.css";
 
 const ContactWindow = () => {
   return (
-    <div className="card">
-      <div className="contactTItle">Contact</div>
-      <div className="cardBody">
-        <div className="contactText">
-          <p>
-            You can contact me through email or drop a message on LinkedIn. I'll
-            get back to you as soon as possible!
-          </p>
-          <p>
-            Message me on <a href="">LinkedIn</a> <br />
-            Or click the button below to send me an email.
-          </p>
-          <a href="mailto:oscarrapale@gmail.com">Send Email</a>
+    <div className={styles.cardContainer}>
+      <div className="card">
+        <div className={styles.contactTitle}>Contact</div>
+        <div className={styles.line}></div>
+        <div className={styles.cardBody}>
+          <div className={styles.cardText}>
+            <p>
+              You can contact me through email or drop a message on LinkedIn.{" "}
+              <br />
+              I'll get back to you as soon as possible!
+            </p>
+            <br />
+            <p>
+              Message me on <a href="">LinkedIn</a> <br />
+              Or click the button below to send me an email.
+            </p>
+            <br />
+            <a className={styles.emailBtn} href="mailto:oscarrapale@gmail.com">
+              Send Email
+            </a>
+            <br />
+            <br />
+          </div>
         </div>
       </div>
     </div>
