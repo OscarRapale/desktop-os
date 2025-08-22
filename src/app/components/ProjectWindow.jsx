774536b63fb4bd36555222c1e338881c76ae003d
OscarRapale <oscarrapale@gmail.com>
2025-08-22 13:32:29 -0400

    Enhance ProjectWindow component: add Flask to skills, update image dimensions, and improve styling with hover effects

--- src/app/components/ProjectWindow.jsx
+++ src/app/components/ProjectWindow.jsx
@@ -23,6 +23,7 @@ const ProjectWindow = () => {
               <li>MongoDB</li>
               <li>PostreSQL</li>
               <li>Python</li>
+              <li>Flask</li>
             </ul>
           </div>
 
@@ -43,15 +44,15 @@ const ProjectWindow = () => {
                 <Image
                   src="/images/the-line.webp"
                   alt="Screen capture of The Line Homepage"
-                  width={300}
-                  height={250}
+                  width={315}
+                  height={200}
                 />
               </div>
               <div className={styles.projectContent}>
                 <h3>The Line - Tribute to The Bear TV Series</h3>
                 <p>
-                  A modern, responsive restaurant website for "The Line", a fine
-                  dining establishment in Chicago's River North neighborhood.
+                  A modern, responsive restaurant website for "The Line",
+                  a fine dining establishment in Chicago's River North neighborhood. 
                   Built with vanilla HTML, CSS, and JavaScript, featuring
                   elegant design, smooth animations, and comprehensive
                   responsive layouts. A tribute to the tv show "The Bear".{" "}
@@ -63,17 +64,17 @@ const ProjectWindow = () => {
             <div className={styles.shellfolio}>
               <div className={styles.projectImage}>
                 <Image
-                  src="/images/the-line.webp"
-                  alt="Screen capture of Shell Portfolio"
-                  width={300}
-                  height={250}
+                  src="/images/shellfolio.webp"
+                  alt="Screen capture of Shell Portfolio in use"
+                  width={315}
+                  height={200}
                 />
               </div>
               <div className={styles.projectContent}>
                 <h3>Shell Style Portfolio</h3>
                 <p>
                   A portfolio website that simulates a terminal interface, built
-                  with vanilla JavaScript, HTML, and CSS. Experience my
+                  with vanilla JavaScript, <br /> HTML, and CSS. Experience my
                   professional information through command-line interactions!{" "}
                   <br />
                   <a href="">Visit my Shell Style Portfolio</a>
@@ -83,17 +84,17 @@ const ProjectWindow = () => {
             <div className={styles.simplePortfolio}>
               <div className={styles.projectImage}>
                 <Image
-                  src="/images/the-line.webp"
-                  alt="Screen capture of The Line Homepage"
-                  width={300}
-                  height={250}
+                  src="/images/portfolio.webp"
+                  alt="Screen capture of The hero section of my personal portfolio"
+                  width={315}
+                  height={200}
                 />
               </div>
               <div className={styles.projectContent}>
                 <h3>Simple HTML/CSS Portfolio</h3>
                 <p>
                   This is my personal portfolio website built with HTML,
-                  designed to present my work and capabilities as a developer.
+                  designed to present my work <br /> and capabilities as a developer.
                   The portfolio serves as a digital resume and project showcase.
                   Including smooth animations. <br />
                   <a href="">Visit My Portfolio</a>
@@ -103,18 +104,18 @@ const ProjectWindow = () => {
             <div className={styles.jancuts}>
               <div className={styles.projectImage}>
                 <Image
-                  src="/images/the-line.webp"
-                  alt="Screen capture of The Line Homepage"
-                  width={300}
-                  height={250}
+                  src="/images/jan-cuts.webp"
+                  alt="Screen capture of Jan Cuts professional barber portfolio"
+                  width={315}
+                  height={200}
                 />
               </div>
               <div className={styles.projectContent}>
                 <h3>Jan Cuts Portfolio</h3>
                 <p>
                   A portfolio website that was requested by a professional
-                  barber that wanted to showcase his work and skills and the
-                  services he is offering. The website is made with React.js and
+                  barber that wanted to showcase <br /> his work and skills and the
+                  services he is offering. The website is made with React.js <br /> and
                   Chakra UI, and the UI was designed with Figma. <br />
                   <a href="">Visit Jan Cuts</a>
                 </p>
